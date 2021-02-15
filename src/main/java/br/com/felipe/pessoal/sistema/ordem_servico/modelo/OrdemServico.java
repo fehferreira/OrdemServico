f5e5c95515fac126d5f4bbb5a9352f688aacc0f9
Felipe <felipe.ferreira1003@gmail.com>
2021-02-15 14:26:45 -0300

    Add in OrdemServico entity 3 fields to insert a service description

--- src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/modelo/OrdemServico.java
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/modelo/OrdemServico.java
@@ -12,6 +12,11 @@ public class OrdemServico {
     private Long id;
     private LocalDateTime dataEntrada = LocalDateTime.now();
     private LocalDateTime dataEntrega;
+
+    private String problemaRelatado;
+    private String defeitoEncontrado;
+    private String servicoExecutado;
+
     private BigDecimal valorServico;
 
     @ManyToOne
@@ -42,6 +47,30 @@ public class OrdemServico {
 
     public void setDataEntrega(LocalDateTime dataEntrega) {
         this.dataEntrega = dataEntrega;
+    }
+
+    public String getProblemaRelatado() {
+        return problemaRelatado;
+    }
+
+    public void setProblemaRelatado(String problemaRelatado) {
+        this.problemaRelatado = problemaRelatado;
+    }
+
+    public String getDefeitoEncontrado() {
+        return defeitoEncontrado;
+    }
+
+    public void setDefeitoEncontrado(String defeitoEncontrado) {
+        this.defeitoEncontrado = defeitoEncontrado;
+    }
+
+    public String getServicoExecutado() {
+        return servicoExecutado;
+    }
+
+    public void setServicoExecutado(String servicoExecutado) {
+        this.servicoExecutado = servicoExecutado;
     }
 
     public BigDecimal getValorServico() {
