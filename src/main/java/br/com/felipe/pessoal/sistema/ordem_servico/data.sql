034a2ef0961a852bb912bbf559174501819a3626
Felipe <felipe.ferreira1003@gmail.com>
2021-02-15 14:24:11 -0300

    Create a sql file to populate DB

---
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO CLIENTE(nome, cpf, endereco) VALUES('João Claudio','123456789-01','Rua Jean de la huerta 587');
+INSERT INTO CLIENTE(nome, cpf, endereco) VALUES('Felipe Ferreira','987645321-01','Rua Paulo de Morais 217');
+INSERT INTO CLIENTE(nome, cpf, endereco) VALUES('Maria Clara','852963741-56','Avenida Padre Arlindo Vieira 1089');
+
+INSERT INTO OBJETO(marca, modelo) VALUES('Marelli','IAW 1G7');
+INSERT INTO OBJETO(marca, modelo) VALUES('Bosch','ME 7.9.9');
+INSERT INTO OBJETO(marca, modelo) VALUES('Ford','EEC-V');
+
