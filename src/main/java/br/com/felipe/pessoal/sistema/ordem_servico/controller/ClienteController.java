13df22c132ae91d8012bc310c945c4099091f34d
Felipe <felipe.ferreira1003@gmail.com>
2021-02-15 14:46:33 -0300

    Developed ClienteController, listarClientes method and ClienteDto

---
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/ClienteController.java
@@ -0,0 +1,26 @@
+package br.com.felipe.pessoal.sistema.ordem_servico.controller;
+
+import br.com.felipe.pessoal.sistema.ordem_servico.controller.dto.ClienteDto;
+import br.com.felipe.pessoal.sistema.ordem_servico.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @RequestMapping("/clientes")
+    @ResponseBody
+    public List<ClienteDto> listarClientes(){
+        return ResponseEntity.ok(clienteRepository.findAll()).build();
+    }
+
+}
---
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/dto/ClienteDto.java
@@ -0,0 +1,28 @@
+package br.com.felipe.pessoal.sistema.ordem_servico.controller.dto;
+
+public class ClienteDto {
+
+    private String nome;
+    private String endereco;
+
+    public ClienteDto(String nome, String endereco) {
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
