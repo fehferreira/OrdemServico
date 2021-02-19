d7be929fe307a3e2162b83d1ec1e1b24e5091097
Felipe <felipe.ferreira1003@gmail.com>
2021-02-19 00:38:17 -0300

    Create detalharCliente method to search a ClienteDto on server by Id

--- src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/ClienteController.java
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/ClienteController.java
@@ -1,32 +1,43 @@
 package br.com.felipe.pessoal.sistema.ordem_servico.controller;
 
 import br.com.felipe.pessoal.sistema.ordem_servico.controller.dto.ClienteDto;
+import br.com.felipe.pessoal.sistema.ordem_servico.modelo.Cliente;
 import br.com.felipe.pessoal.sistema.ordem_servico.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
     @Autowired
     private ClienteRepository clienteRepository;
 
-    @RequestMapping("/clientes")
     @ResponseBody
+    @GetMapping("/{nome}")
     public Page<ClienteDto> listarClientes(
-            @RequestParam(required = false) String nome,
+            @RequestParam(required = false) @PathVariable String nome,
             @PageableDefault(sort="id", direction = Sort.Direction.ASC) Pageable paginacao){
         if(nome != null){
             return ClienteDto.converter(clienteRepository.findAll(Sort.by(nome)));
         }
         return ClienteDto.converter(clienteRepository.findAll(paginacao));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClienteDto> detalharCliente(@PathVariable @RequestParam Long id){
+        ClienteDto cliente;
+        try{
+            cliente = new ClienteDto(clienteRepository.findById(id).get());
+        }catch(IllegalArgumentException exception){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cliente);
     }
 
 }
