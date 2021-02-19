b9ce9a9c7b14127dd414b62782a64b7e96ae908e
Felipe <felipe.ferreira1003@gmail.com>
2021-02-19 00:45:46 -0300

    Developed ClienteControllerTest class

---
+++ src/test/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/ClienteControllerTest.java
@@ -0,0 +1,13 @@
+package br.com.felipe.pessoal.sistema.ordem_servico.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class ClienteControllerTest {
+    
+
+}
