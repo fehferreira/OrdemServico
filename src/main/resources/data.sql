e12ee7602910d2e6771e0a61edd2ea0fa75921b4
Felipe <felipe.ferreira1003@gmail.com>
2021-02-19 15:51:43 -0300

    Refact ClienteDto to work with ID value and insert on DB this value with DB startup

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO CLIENTE(nome, cpf, endereco) VALUES('João Claudio','123456789-01','Rua Jean de la huerta 587');
-INSERT INTO CLIENTE(nome, cpf, endereco) VALUES('Felipe Ferreira','987645321-01','Rua Paulo de Morais 217');
-INSERT INTO CLIENTE(nome, cpf, endereco) VALUES('Maria Clara','852963741-56','Avenida Padre Arlindo Vieira 1089');
+INSERT INTO CLIENTE(id, nome, cpf, endereco) VALUES('1','João Claudio','123456789-01','Rua Jean de la huerta 587');
+INSERT INTO CLIENTE(id, nome, cpf, endereco) VALUES('2','Felipe Ferreira','987645321-01','Rua Paulo de Morais 217');
+INSERT INTO CLIENTE(id, nome, cpf, endereco) VALUES('3','Maria Clara','852963741-56','Avenida Padre Arlindo Vieira 1089');
 
-INSERT INTO OBJETO(marca, modelo) VALUES('Marelli','IAW 1G7');
-INSERT INTO OBJETO(marca, modelo) VALUES('Bosch','ME 7.9.9');
-INSERT INTO OBJETO(marca, modelo) VALUES('Ford','EEC-V');
+INSERT INTO OBJETO(id, marca, modelo) VALUES('1','Marelli','IAW 1G7');
+INSERT INTO OBJETO(id, marca, modelo) VALUES('2','Bosch','ME 7.9.9');
+INSERT INTO OBJETO(id, marca, modelo) VALUES('3','Ford','EEC-V');
 
