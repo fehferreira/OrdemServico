1416c7a3c96a238baa3b9fb061680491cc50d50b
Felipe <felipe.ferreira1003@gmail.com>
2021-02-19 01:07:48 -0300

    Develop method to test return from get requisition to get List of Clientes

--- src/test/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/ClienteControllerTest.java
+++ src/test/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/ClienteControllerTest.java
@@ -1,13 +1,32 @@
 package br.com.felipe.pessoal.sistema.ordem_servico.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.net.URI;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 class ClienteControllerTest {
-    
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void deveriaValidarARequisicaoDeListaDeClientes() throws Exception {
+        URI uri = new URI("/clientes");
+
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
+                .post(uri))
+                .andExpect(MockMvcResultMatchers.status().is(200));
+    }
 }
