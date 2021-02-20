8a6d013dd16292d057574393d0fcfd7d5f5a86c1
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-02-19 21:58:36 -0300

    Developed test method to validate all recieve values from GET Http method call

--- src/test/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/ClienteControllerTest.java
+++ src/test/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/ClienteControllerTest.java
@@ -1,32 +1,79 @@
 package br.com.felipe.pessoal.sistema.ordem_servico.controller;
 
+import br.com.felipe.pessoal.sistema.ordem_servico.modelo.Cliente;
+import br.com.felipe.pessoal.sistema.ordem_servico.repository.ClienteRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.net.URI;
+import java.util.Locale;
+import java.util.Optional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
-class ClienteControllerTest {
+class ClienteControllerTest{
+
+    private final String uriClientes = "/clientes";
+
+    private Faker faker = new Faker(new Locale("pt-BR"));
+
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private ClienteController clienteController;
 
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @BeforeTestMethod
+    public void setup(){
+        objectMapper = new ObjectMapper();
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(clienteController)
+                .build();
+    }
+
     @Test
     public void deveriaValidarARequisicaoDeListaDeClientes() throws Exception {
-        URI uri = new URI("/clientes");
+        URI uri = new URI(uriClientes);
 
-        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
+        mockMvc.perform(MockMvcRequestBuilders
                 .post(uri))
                 .andExpect(MockMvcResultMatchers.status().is(200));
+    }
+
+    @Test
+    public void deveriaValidarOsValoresRecebidosDaListaDeClientes() throws Exception {
+        String name = faker.name().toString();
+        String cpf = String.valueOf(faker.number().numberBetween(111111111,999999999));
+        String address = faker.address().fullAddress().toString();
+
+        Cliente cliente = new Cliente(name,cpf,address);
+        clienteRepository.save(cliente);
+
+        Mockito.when(clienteRepository.findById(cliente.getId())).thenReturn(Optional.of(cliente));
+
+        mockMvc.perform(MockMvcRequestBuilders.get(uriClientes))
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(200))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(cliente.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.nome").value(cliente.getNome()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.endereco").value(cliente.getEndereco()));
+
+        Mockito.verify(clienteRepository,Mockito.times(1)).findById(cliente.getId());
     }
 }
