4105f246bb99a67b5f6a42418747ff8f0ce63de3
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-02-25 20:06:43 -0300

    Cleaning static parameters and imports

--- src/test/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/ClienteControllerTest.java
+++ src/test/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/ClienteControllerTest.java
@@ -2,27 +2,18 @@ package br.com.felipe.pessoal.sistema.ordem_servico.controller;
 
 import br.com.felipe.pessoal.sistema.ordem_servico.modelo.Cliente;
 import br.com.felipe.pessoal.sistema.ordem_servico.repository.ClienteRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.javafaker.Faker;
-import org.aspectj.lang.annotation.Before;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
-import org.springframework.test.context.event.annotation.BeforeTestMethod;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Locale;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -30,26 +21,11 @@ class ClienteControllerTest{
 
     private final String uriClientes = "/clientes";
 
-    private Faker faker = new Faker(new Locale("pt-BR"));
-
-    private ObjectMapper objectMapper;
-
-    @Autowired
-    private ClienteController clienteController;
-
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ClienteRepository clienteRepository;
-
-    @BeforeTestClass
-    public void setup(){
-        objectMapper = new ObjectMapper();
-        mockMvc = MockMvcBuilders
-                .standaloneSetup(clienteController)
-                .build();
-    }
 
     @Test
     public void deveriaValidarARequisicaoDeListaDeClientes() throws Exception {
