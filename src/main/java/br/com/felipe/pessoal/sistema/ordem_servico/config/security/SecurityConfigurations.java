91661e36c83a5fc4504484a4635c9e3aea39d9a7
Felipe <felipe.ferreira1003@gmail.com>
2021-03-09 12:39:37 -0300

    Insert SpringBoot Security dependece, create SecurityConfigurations class to configure all configs of security and enable acess to "/clientes" endpoint

---
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/config/security/SecurityConfigurations.java
@@ -0,0 +1,29 @@
+package br.com.felipe.pessoal.sistema.ordem_servico.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/clientes").permitAll()
+                .antMatchers(HttpMethod.GET,"/clientes/*").permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+    }
+}
