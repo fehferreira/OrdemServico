e25fb2b7414215dfaee2e72583018f83bb101b2d
Felipe <felipe.ferreira1003@gmail.com>
2021-03-09 12:58:38 -0300

    Configure implements methods in Usuario entity and create Perfil class to define a profile os security in project

--- src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/modelo/Usuario.java
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/modelo/Usuario.java
@@ -1,18 +1,25 @@
 package br.com.felipe.pessoal.sistema.ordem_servico.modelo;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.hibernate.cfg.annotations.reflection.PersistentAttributeFilter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String email;
     private String senha;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Perfil> perfis = new ArrayList<>();
 
     public String getEmail() {
         return email;
@@ -28,5 +35,40 @@ public class Usuario {
 
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.perfis;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
