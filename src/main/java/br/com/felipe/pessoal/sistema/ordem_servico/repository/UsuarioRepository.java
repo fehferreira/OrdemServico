c56ef343cc133eaca8e47304ce73c6de6dc31b9c
Felipe <felipe.ferreira1003@gmail.com>
2021-03-09 14:09:49 -0300

    Refact findByEmail method on Usuario repository and developed loadUserByUsername method on AutenticacaoService class

--- src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/repository/UsuarioRepository.java
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/repository/UsuarioRepository.java
@@ -3,8 +3,10 @@ package br.com.felipe.pessoal.sistema.ordem_servico.repository;
 import br.com.felipe.pessoal.sistema.ordem_servico.modelo.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    public Usuario findByEmail(String email);
+    public Optional<Usuario> findByEmail(String email);
 
 }
