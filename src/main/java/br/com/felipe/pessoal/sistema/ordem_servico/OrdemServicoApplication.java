3e6a17e266df686c5f6e257e321dbe1674c195a3
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-03-09 22:22:36 -0300

    Change populateDB method of OrdemServicoApplication to HomeController class

--- src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/OrdemServicoApplication.java
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/OrdemServicoApplication.java
@@ -1,12 +1,5 @@
 package br.com.felipe.pessoal.sistema.ordem_servico;
 
-import br.com.felipe.pessoal.sistema.ordem_servico.modelo.Cliente;
-import br.com.felipe.pessoal.sistema.ordem_servico.modelo.Objeto;
-import br.com.felipe.pessoal.sistema.ordem_servico.modelo.Usuario;
-import br.com.felipe.pessoal.sistema.ordem_servico.repository.ClienteRepository;
-import br.com.felipe.pessoal.sistema.ordem_servico.repository.ObjetoRepository;
-import br.com.felipe.pessoal.sistema.ordem_servico.repository.UsuarioRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
@@ -14,36 +7,8 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 @SpringBootApplication
 @EnableSpringDataWebSupport
 public class OrdemServicoApplication {
-
-	@Autowired
-	private UsuarioRepository usuarioRepository;
-
-	@Autowired
-	private ClienteRepository clienteRepository;
-
-	@Autowired
-	private ObjetoRepository objetoRepository;
-
 	public static void main(String[] args) {
-
-		populateDB();
-
 		SpringApplication.run(OrdemServicoApplication.class, args);
-	}
-
-	private static void populateDB() {
-		Usuario usuario1 = new Usuario("joao_claudio@email.com","123456");
-		Usuario usuario2 = new Usuario("felipe_ferreira@email.com","987654");
-		Usuario usuario3 = new Usuario("maria_clara@email.com","123789");
-
-		Cliente cliente1 = new Cliente("João Claudio", "123456789-01", "Rua Jean de la huerta 587", usuario1);
-		Cliente cliente2 = new Cliente("Felipe Ferreira", "987645321-01", "Rua Paulo de Morais 217", usuario2);
-		Cliente cliente3 = new Cliente("Maria Clara", "852963741-56", "Avenida Padre Arlindo Vieira 1089", usuario3);
-
-		Objeto objeto1 = new Objeto("Marelli", "IAW 1G7");
-		Objeto objeto2 = new Objeto("Bosch", "ME 7.9.9");
-		Objeto objeto3 = new Objeto("Ford", "EEC-V");
-
 	}
 
 }
--- src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/HomeController.java
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/HomeController.java
@@ -1,5 +1,12 @@
 package br.com.felipe.pessoal.sistema.ordem_servico.controller;
 
+import br.com.felipe.pessoal.sistema.ordem_servico.modelo.Cliente;
+import br.com.felipe.pessoal.sistema.ordem_servico.modelo.Objeto;
+import br.com.felipe.pessoal.sistema.ordem_servico.modelo.Usuario;
+import br.com.felipe.pessoal.sistema.ordem_servico.repository.ClienteRepository;
+import br.com.felipe.pessoal.sistema.ordem_servico.repository.ObjetoRepository;
+import br.com.felipe.pessoal.sistema.ordem_servico.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +14,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private ObjetoRepository objetoRepository;
+
     @RequestMapping("/")
     @ResponseBody
     public String home(){
+        populateDB();
+
         return "HOME PAGE";
+    }
+
+    private void populateDB() {
+        Usuario usuario1 = new Usuario("joao_claudio@email.com","123456");
+        Usuario usuario2 = new Usuario("felipe_ferreira@email.com","987654");
+        Usuario usuario3 = new Usuario("maria_clara@email.com","123789");
+
+        Cliente cliente1 = new Cliente("João Claudio", "123456789-01", "Rua Jean de la huerta 587", usuario1);
+        Cliente cliente2 = new Cliente("Felipe Ferreira", "987645321-01", "Rua Paulo de Morais 217", usuario2);
+        Cliente cliente3 = new Cliente("Maria Clara", "852963741-56", "Avenida Padre Arlindo Vieira 1089", usuario3);
+
+        Objeto objeto1 = new Objeto("Marelli", "IAW 1G7");
+        Objeto objeto2 = new Objeto("Bosch", "ME 7.9.9");
+        Objeto objeto3 = new Objeto("Ford", "EEC-V");
+
     }
 
 }
