4e25a561fc857b40144b347de324c16f4a9ac91e
Felipe <felipe.ferreira1003@gmail.com>
2021-03-11 13:27:54 -0300

    Developed converter method on LoginForm to return a UserNamePasswordAuthenticationToken. Create a algorithm on autentica method to authenticate a requisition with email and password.

--- src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/AutenticacaoController.java
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/AutenticacaoController.java
@@ -4,6 +4,9 @@ import br.com.felipe.pessoal.sistema.ordem_servico.controller.form.LoginForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +21,23 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager authManager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity<?> autenticar(@RequestBody @Valid LoginForm form){
-        System.out.println(form.getEmail());
-        System.out.println(form.getSenha());
 
-        return ResponseEntity.ok().build();
+        try {
+            UsernamePasswordAuthenticationToken dadosLogin = form.converter();
+            Authentication authentication = authManager.authenticate(dadosLogin);
+
+            String token = tokenService.gerarToken(authentication);
+
+            return ResponseEntity.ok().build();
+        }catch (AuthenticationException e){
+            return ResponseEntity.badRequest().build();
+        }
+
     }
 
 }
