28325e91444d48a0819be3b257e2fb648a2c7b8b
Felipe <felipe.ferreira1003@gmail.com>
2021-03-12 15:23:22 -0300

    Create a filter to validate  a recieve token and implements on configuration method on SecurityConfigurations class

---
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/config/security/AutenticacaoViaTokenFilter.java
@@ -0,0 +1,27 @@
+package br.com.felipe.pessoal.sistema.ordem_servico.config.security;
+
+import org.hibernate.annotations.Filter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = recuperarToken(request);
+        System.out.println(token);
+
+        filterChain.doFilter(request,response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        if(token == null || token.isEmpty() || !token.startsWith("Bearer "))
+            return null;
+        return token.substring(7,token.length());
+    }
+}
