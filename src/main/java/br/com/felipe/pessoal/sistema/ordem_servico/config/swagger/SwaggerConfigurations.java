2928ad709b83d6e1fe8abadf913afe3ba212e0db
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-03-18 20:10:46 -0300

    Inserting a parameter to recieve a JWT to authenticate a requisition

--- src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/config/swagger/SwaggerConfigurations.java
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/config/swagger/SwaggerConfigurations.java
@@ -6,8 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ScalarType;
+import springfox.documentation.service.ParameterType;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import static java.util.Collections.singletonList;
 
 @Configuration
 public class SwaggerConfigurations {
@@ -19,7 +23,14 @@ public class SwaggerConfigurations {
                 .apis(RequestHandlerSelectors.basePackage("br.com.felipe.pessoal.sistema.ordem_servico"))
                 .paths(PathSelectors.ant("/**"))
                 .build()
-                .ignoredParameterTypes(Cliente.class, Perfil.class);
+                .ignoredParameterTypes(Cliente.class, Perfil.class)
+                .globalRequestParameters(singletonList(new springfox.documentation.builders.RequestParameterBuilder()
+                        .name("Authorization")
+                        .description("Header para token JWT")
+                        .in(ParameterType.HEADER)
+                        .required(false)
+                        .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+                        .build()));
     }
 
 }
