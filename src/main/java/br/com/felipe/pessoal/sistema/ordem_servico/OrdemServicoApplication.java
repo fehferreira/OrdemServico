9b297d9e169c4f468aa040b19674a3d60986a538
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-03-18 17:45:57 -0300

    Configuring Swagger to Documentation and create a class to configure all functions

--- src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/OrdemServicoApplication.java
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/OrdemServicoApplication.java
@@ -3,9 +3,11 @@ package br.com.felipe.pessoal.sistema.ordem_servico;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
+@EnableSwagger2
 public class OrdemServicoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrdemServicoApplication.class, args);
