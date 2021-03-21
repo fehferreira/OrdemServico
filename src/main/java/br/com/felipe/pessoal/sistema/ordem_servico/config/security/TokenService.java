1be6ae6b098f95c8e4434f0ef1c0b330ff08ecc0
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-03-21 17:05:32 -0300

    Create a isTokenValido method to validate a recieve token

--- src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/config/security/TokenService.java
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/config/security/TokenService.java
@@ -31,4 +31,14 @@ public class TokenService {
                 .signWith(SignatureAlgorithm.HS256, secret)
                 .compact();
     }
+
+    public boolean isTokenValido(String token) {
+        try {
+            Jwts.parser().setSigningKey(this.secret)
+                    .parseClaimsJws(token);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
 }
