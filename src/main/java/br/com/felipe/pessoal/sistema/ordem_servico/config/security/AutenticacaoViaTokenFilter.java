a09c09a2f3ae0be0c5eaee92d7ea1156dc8f05a7
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-03-21 17:03:53 -0300

    Refact AutenticacaoViaTokenFilter to recieve a dependence injection

--- src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/config/security/AutenticacaoViaTokenFilter.java
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/config/security/AutenticacaoViaTokenFilter.java
@@ -10,11 +10,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
+
+    private TokenService tokenService;
+
+    public AutenticacaoViaTokenFilter(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = recuperarToken(request);
         System.out.println(token);
+        if(tokenService.isTokenValido(token)){
 
+        }
         filterChain.doFilter(request,response);
     }
 
--- src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/config/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.felipe.pessoal.sistema.ordem_servico.config.security;
 
+import antlr.Token;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +22,9 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     @Autowired
     private AutenticacaoService autenticacaoService;
 
+    @Autowired
+    private TokenService tokenService;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
@@ -35,7 +39,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().addFilterBefore(new AutenticacaoViaTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+                .and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
