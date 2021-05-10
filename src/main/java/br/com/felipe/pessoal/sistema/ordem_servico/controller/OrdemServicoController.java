1df78d30d7b01042ceb2fba34424fd69001a8250
Felipe <felipe.ferreira1003@gmail.com>
2021-05-10 16:45:54 -0300

    Refact return of exibirOrdens method to a Page and receives a Pageable on signature

--- src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/OrdemServicoController.java
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/OrdemServicoController.java
@@ -3,7 +3,9 @@ package br.com.felipe.pessoal.sistema.ordem_servico.controller;
 import br.com.felipe.pessoal.sistema.ordem_servico.controller.dto.OrdemDTO;
 import br.com.felipe.pessoal.sistema.ordem_servico.repository.OrdemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +18,8 @@ public class OrdemServicoController {
     private OrdemRepository ordemRepository;
 
     @GetMapping
-    public ResponseEntity<OrdemDTO> exibirOrdens(){
-        return OrdemDTO.converterOrdens(ordemRepository.findAll());
+    public Page<OrdemDTO> exibirOrdens(@PageableDefault Pageable paginacao){
+        return OrdemDTO.converterOrdens(ordemRepository.findAll(paginacao));
     }
 
 }
