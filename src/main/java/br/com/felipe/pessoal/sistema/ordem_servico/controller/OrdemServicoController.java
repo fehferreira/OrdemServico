78292ceea6c90636765dd1c0b2e136cf207342d1
Felipe <felipe.ferreira1003@gmail.com>
2021-05-10 16:35:58 -0300

    Create controller and exibirOrdens method

---
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/OrdemServicoController.java
@@ -0,0 +1,21 @@
+package br.com.felipe.pessoal.sistema.ordem_servico.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/servicos")
+public class OrdemServicoController {
+
+    @Autowired
+    private OrdemRepository ordemRepository;
+
+    @GetMapping
+    public ResponseEntity<OrdemDTO> exibirOrdens(){
+        return OrdemDTO.converterOrdens(ordemRepository.findAll());
+    }
+
+}
