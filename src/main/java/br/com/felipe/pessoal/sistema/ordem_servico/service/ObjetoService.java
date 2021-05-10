0ea9dee9d9242c3bd52b31d13ff7bcb60b1cee56
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-05-10 20:11:28 -0300

    Developed deletarObjeto method on objetoService class

--- src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/service/ObjetoService.java
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/service/ObjetoService.java
@@ -5,11 +5,13 @@ import br.com.felipe.pessoal.sistema.ordem_servico.controller.form.ObjetoCadastr
 import br.com.felipe.pessoal.sistema.ordem_servico.modelo.Objeto;
 import br.com.felipe.pessoal.sistema.ordem_servico.repository.ObjetoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Optional;
 
 @Service
 public class ObjetoService {
@@ -26,5 +28,14 @@ public class ObjetoService {
         }
         URI uri =uriBuilder.path("/aparelho/${id}").buildAndExpand(novoObjeto.getId()).toUri();
         return ResponseEntity.created(uri).body(new ObjetoDTO(novoObjeto));
+    }
+
+    public ResponseEntity<ObjetoDTO> deletarObjeto(Long id) {
+        Optional<Objeto> objeto = objetoRepository.findById(id);
+        if(objeto.isPresent()) {
+            objetoRepository.delete(objeto.get());
+            return ResponseEntity.status(HttpStatus.OK).build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
