03c1110d31de3ea43fcf9fe8170022587d5f2cc7
Felipe <felipe.ferreira1003@gmail.com>
2021-05-10 17:17:51 -0300

    Create a signature of cadastrarObjeto method

--- src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/ObjetoController.java
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/ObjetoController.java
@@ -6,8 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Controller
 @RequestMapping("/aparelho")
@@ -16,8 +21,14 @@ public class ObjetoController {
     @Autowired
     private ObjetoRepository objetoRepository;
 
+    @GetMapping
     public Page<ObjetoDTO> exibirAparelhos(@PageableDefault Pageable paginacao){
         return ObjetoDTO.converterObjetos(objetoRepository.findAll(paginacao));
+    }
+
+    @PostMapping
+    public ResponseEntity<ObjetoDTO> cadastrarObjeto(@RequestBody ObjetoCadastradoForm form, UriComponentsBuilder uri){
+
     }
 
 
