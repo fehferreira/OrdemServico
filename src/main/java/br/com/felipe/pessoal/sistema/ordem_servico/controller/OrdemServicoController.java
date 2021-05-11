ad3ffe274ead2ad99bbb67c757aeb052bca29724
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-05-10 22:03:20 -0300

    Develop exibirOrdens on OrdemServicoService class

--- src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/OrdemServicoController.java
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/OrdemServicoController.java
@@ -17,9 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class OrdemServicoController {
 
     @Autowired
-    private OrdemRepository ordemRepository;
-
-    @Autowired
     private OrdemServicoService ordemService;
 
     @GetMapping
--- src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/service/OrdemServicoService.java
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/service/OrdemServicoService.java
@@ -1,7 +1,19 @@
 package br.com.felipe.pessoal.sistema.ordem_servico.service;
 
+import br.com.felipe.pessoal.sistema.ordem_servico.controller.dto.OrdemDTO;
+import br.com.felipe.pessoal.sistema.ordem_servico.repository.OrdemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrdemServicoService {
+
+    @Autowired
+    private OrdemRepository ordemRepository;
+
+    public Page<OrdemDTO> exibirOrdens(Pageable paginacao) {
+        return OrdemDTO.converterOrdens(ordemRepository.findAll(paginacao));
+    }
 }
