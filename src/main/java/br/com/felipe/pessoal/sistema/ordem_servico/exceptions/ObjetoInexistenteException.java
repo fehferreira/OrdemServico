f66e295cc685247025fbc789abcf11462074e3b0
Felipe <felipe.ferreira1003@gmail.com>
2021-06-04 17:31:46 -0300

    Develop 2 test method to deletarObjeto method and create a unique exception

---
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/exceptions/ObjetoInexistenteException.java
@@ -0,0 +1,7 @@
+package br.com.felipe.pessoal.sistema.ordem_servico.exceptions;
+
+public class ObjetoInexistenteException extends RuntimeException {
+    public ObjetoInexistenteException(String message) {
+        super(message);
+    }
+}
--- src/test/java/br/com/felipe/pessoal/sistema/ordem_servico/service/ObjetoServiceTest.java
+++ src/test/java/br/com/felipe/pessoal/sistema/ordem_servico/service/ObjetoServiceTest.java
@@ -3,6 +3,7 @@ package br.com.felipe.pessoal.sistema.ordem_servico.service;
 import br.com.felipe.pessoal.sistema.ordem_servico.controller.dto.ObjetoDTO;
 import br.com.felipe.pessoal.sistema.ordem_servico.controller.form.ObjetoCadastradoForm;
 import br.com.felipe.pessoal.sistema.ordem_servico.exceptions.ObjetoExistenteException;
+import br.com.felipe.pessoal.sistema.ordem_servico.exceptions.ObjetoInexistenteException;
 import br.com.felipe.pessoal.sistema.ordem_servico.modelo.Objeto;
 import br.com.felipe.pessoal.sistema.ordem_servico.repository.ObjetoRepository;
 import org.junit.jupiter.api.Test;
@@ -10,10 +11,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.internal.verification.VerificationModeFactory;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -65,6 +69,29 @@ class ObjetoServiceTest {
         }catch (Exception exception){
             assertEquals(ObjetoExistenteException.class, exception.getClass());
         }
+    }
+
+    @Test
+    void enviaUmId_retornaHttpStatus200(){
+        Objeto objetoSalvo = new Objeto("Delphi", "MT60");
+        objetoSalvo.setId(1L);
+        Mockito.when(objetoRepositoryMock.findById(objetoSalvo.getId())).thenReturn(Optional.of(objetoSalvo));
+
+        ResponseEntity<ObjetoDTO> response = objetoService.deletarObjeto(objetoSalvo.getId());
+
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+    }
+
+    @Test
+    void enviaUmIdDeUmObjetoInexistente_retornaUmaExceptionsEspecial(){
+        try{
+            ResponseEntity<ObjetoDTO> response = objetoService.deletarObjeto(1L);
+        }catch(ObjetoInexistenteException exception){
+            assertEquals(ObjetoInexistenteException.class, exception.getClass());
+        }
+
+        Mockito.verify(objetoRepositoryMock).findById(Mockito.any());
+        Mockito.verifyNoMoreInteractions(objetoRepositoryMock);
     }
     
 }
