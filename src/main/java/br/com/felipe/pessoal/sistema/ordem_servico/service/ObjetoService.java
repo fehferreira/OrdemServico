c26dc091d9511b86875a51cde40ee3f378dc6815
Felipe <felipe.ferreira1003@gmail.com>
2021-06-04 17:38:59 -0300

    Refact return of values from ObjetoService to a DTO objects

--- src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/service/ObjetoService.java
+++ src/main/java/br/com/felipe/pessoal/sistema/ordem_servico/service/ObjetoService.java
@@ -4,6 +4,7 @@ import br.com.felipe.pessoal.sistema.ordem_servico.controller.dto.ObjetoDTO;
 import br.com.felipe.pessoal.sistema.ordem_servico.controller.form.ObjetoAtualizadoForm;
 import br.com.felipe.pessoal.sistema.ordem_servico.controller.form.ObjetoCadastradoForm;
 import br.com.felipe.pessoal.sistema.ordem_servico.exceptions.ObjetoExistenteException;
+import br.com.felipe.pessoal.sistema.ordem_servico.exceptions.ObjetoInexistenteException;
 import br.com.felipe.pessoal.sistema.ordem_servico.modelo.Objeto;
 import br.com.felipe.pessoal.sistema.ordem_servico.repository.ObjetoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,36 +26,33 @@ public class ObjetoService {
         this.objetoRepository = objetoRepository;
     }
 
-    public ResponseEntity<ObjetoDTO> cadastrarObjeto(ObjetoCadastradoForm formObjeto, UriComponentsBuilder uriBuilder) {
+    public ObjetoDTO cadastrarObjeto(ObjetoCadastradoForm formObjeto, UriComponentsBuilder uriBuilder) {
         Objeto novoObjeto = formObjeto.retornarObjeto();
-
         if(objetoRepository.findByMarcaAndModelo(novoObjeto).isPresent()){
             throw new ObjetoExistenteException("Objeto já existe no Banco de Dados.");
         }
-
         try{
             novoObjeto = objetoRepository.save(novoObjeto);
         }catch (IllegalArgumentException exception){
             throw new IllegalArgumentException("Impossível salvar este usuário.",exception);
         }
-        URI uri =uriBuilder.path("/aparelho/${id}").buildAndExpand(novoObjeto.getId()).toUri();
-        return ResponseEntity.created(uri).body(new ObjetoDTO(novoObjeto));
+        return new ObjetoDTO(novoObjeto);
     }
 
-    public ResponseEntity<ObjetoDTO> deletarObjeto(Long id) {
+    public boolean deletarObjeto(Long id) {
         Optional<Objeto> objeto = objetoRepository.findById(id);
         if(objeto.isEmpty()) {
-            return ResponseEntity.notFound().build();
+            throw new ObjetoInexistenteException("Este objeto não existe no Banco de Dados.");
         }
         objetoRepository.delete(objeto.get());
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return true;
     }
 
-    public ResponseEntity<ObjetoDTO> alterarObjeto(ObjetoAtualizadoForm formAtualizado, UriComponentsBuilder uri) {
+    public ObjetoDTO alterarObjeto(ObjetoAtualizadoForm formAtualizado, UriComponentsBuilder uri) {
         Optional<Objeto> optional = objetoRepository.findById(formAtualizado.getId());
         if(optional.isPresent()){
-            return ResponseEntity.ok(new ObjetoDTO(formAtualizado.atualizar(formAtualizado.getId(), objetoRepository)));
+            throw new ObjetoInexistenteException("Este objeto não existe no Banco de Dados.");
         }
-        return ResponseEntity.notFound().build();
+        return new ObjetoDTO(formAtualizado.atualizar(formAtualizado.getId(), objetoRepository));
     }
 }
