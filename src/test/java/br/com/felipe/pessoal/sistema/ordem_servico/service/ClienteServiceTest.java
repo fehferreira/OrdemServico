f4e26c4feb03988368685b84aa23dc31fcc9e948
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-06-07 19:40:45 -0300

    Created test method who expected a cliente list

--- src/test/java/br/com/felipe/pessoal/sistema/ordem_servico/service/ClienteServiceTest.java
+++ src/test/java/br/com/felipe/pessoal/sistema/ordem_servico/service/ClienteServiceTest.java
@@ -1,12 +1,17 @@
 package br.com.felipe.pessoal.sistema.ordem_servico.service;
 
+import br.com.felipe.pessoal.sistema.ordem_servico.modelo.Cliente;
 import br.com.felipe.pessoal.sistema.ordem_servico.repository.ClienteRepository;
-import br.com.felipe.pessoal.sistema.ordem_servico.repository.ObjetoRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,6 +26,30 @@ class ClienteServiceTest {
 
     private UriComponentsBuilder uriBuilder = UriComponentsBuilder.newInstance();
 
+    @Test
+    void solicitaUmaListaDeClientesSemFiltroDeNome_retornaUmaListaDeClientes(){
+        List<Cliente> clientes = criaListaClientes();
+
+        Mockito.when(clienteRepositoryMock.findAll()).thenReturn(clientes);
+        List<Cliente> listaRetorno = clienteService.listarClientes(null);
+
+        assertEquals(clientes, listaRetorno);
+
+    }
+
     
+
+    private List<Cliente> criaListaClientes(){
+        Cliente cliente1 = new Cliente("Felipe Ferreira", "12345678901", "Rua dos Jequitibás");
+        Cliente cliente2 = new Cliente("Maria Antonieta", "98765432101", "Avenida dos Manguaris");
+        Cliente cliente3 = new Cliente("Rafael Carlos Dias", "78965412303", "Travessa dos Vizinhos");
+
+        cliente1.setId(1L);
+        cliente1.setId(2L);
+        cliente1.setId(3L);
+
+        List<Cliente> clientes = List.of(cliente1,cliente2,cliente3);
+        return clientes;
+    }
 
 }
