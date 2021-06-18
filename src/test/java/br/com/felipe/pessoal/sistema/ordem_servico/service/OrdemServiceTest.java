d2e43c2498324ce8063616a58e76ee15d2755de5
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-06-17 23:57:11 -0300

    Develop another 3 test methods

--- src/test/java/br/com/felipe/pessoal/sistema/ordem_servico/service/OrdemServiceTest.java
+++ src/test/java/br/com/felipe/pessoal/sistema/ordem_servico/service/OrdemServiceTest.java
@@ -11,11 +11,14 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import javax.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
 class OrdemServiceTest {
@@ -27,8 +30,8 @@ class OrdemServiceTest {
     private OrdemServicoService ordemService;
 
     @Test
-    void solicitaTodasAsOrdensDeServico_retonaUmaListaDeOrdens(){
-        List<OrdemServico> ordens = criarOrdemServico();
+    void solicitaTodasAsOrdensDeServico_retornaUmaListaDeOrdens(){
+        List<OrdemServico> ordens = criarListaOrdemServico();
 
         Mockito.when(ordemRepositoryMock.findAll()).thenReturn(ordens);
         List<OrdemServico> ordensRetorno = ordemService.exibirOrdens();
@@ -41,7 +44,44 @@ class OrdemServiceTest {
 
     }
 
-    private List<OrdemServico> criarOrdemServico(){
+    @Test
+    void solicitaAsOrdensDeServicoDeUmDBVazio_retornaUmaListaVazia(){
+        List<OrdemServico> ordemServicos = ordemService.exibirOrdens();
+        assertTrue(ordemServicos.isEmpty());
+    }
+
+    @Test
+    void solicitaADelecaoDeUmaOrdem_retornaUmaOrdemDeletada(){
+        OrdemServico ordem = criarOrdemServico();
+        Mockito.when(ordemRepositoryMock.findById(ordem.getId())).thenReturn(Optional.of(ordem));
+        OrdemServico retornoOrdem = ordemService.deletarOrdem(ordem.getId());
+
+        assertEquals(ordem.getId(), retornoOrdem.getId());
+        assertEquals(ordem.getDataEntrada(), retornoOrdem.getDataEntrada());
+        assertEquals(ordem.getDataEntrega(), retornoOrdem.getDataEntrega());
+        assertEquals(ordem.getCliente(), retornoOrdem.getCliente());
+        assertEquals(ordem.getAparelho(), retornoOrdem.getAparelho());
+    }
+
+    @Test
+    void solicitaADelecaoDeUmaOrdemInexistente_retornaUmaExcessao(){
+        Mockito.when(ordemRepositoryMock.findById(Mockito.any())).thenThrow(EntityNotFoundException.class);
+        try {
+            ordemService.deletarOrdem(1L);
+        }catch (Exception exception){
+            assertEquals(EntityNotFoundException.class, exception.getClass());
+        }
+        Mockito.verify(ordemRepositoryMock).findById(Mockito.any());
+    }
+
+    private OrdemServico criarOrdemServico() {
+        Cliente cliente = new Cliente("Felipe Ferreira", "12345678901", "Av. Tamanduateí 58");
+        Objeto objeto = new Objeto(5L, "Marelli", "IAW 7GF");
+        return new OrdemServico(LocalDateTime.now(), LocalDateTime.now().plusDays(2L), cliente, objeto);
+    }
+
+
+    private List<OrdemServico> criarListaOrdemServico(){
         Cliente cliente1 = new Cliente("Felipe", "12398745698", "Av. dos tamanduateís 54");
         Cliente cliente2 = new Cliente("Armando", "97854589654", "Rua das tramóias 948");
         cliente1.setId(1L);
