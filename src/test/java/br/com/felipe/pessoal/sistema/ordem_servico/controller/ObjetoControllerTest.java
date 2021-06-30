4fb54a88cb77e69dd5aa06e51d03be255f5eef75
fehferreira <felipe.ferreira1003@gmail.com>
2021-06-30 18:15:55 -0300

    Develop test methods to ObjetoController class

--- src/test/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/ObjetoControllerTest.java
+++ src/test/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/ObjetoControllerTest.java
@@ -1,15 +1,20 @@
 package br.com.felipe.pessoal.sistema.ordem_servico.controller;
 
 import br.com.felipe.pessoal.sistema.ordem_servico.controller.dto.ObjetoDTO;
+import br.com.felipe.pessoal.sistema.ordem_servico.controller.form.ObjetoCadastradoForm;
+import br.com.felipe.pessoal.sistema.ordem_servico.exceptions.NenhumObjetoCadastradoException;
+import br.com.felipe.pessoal.sistema.ordem_servico.exceptions.ObjetoExistenteException;
 import br.com.felipe.pessoal.sistema.ordem_servico.modelo.Objeto;
 import br.com.felipe.pessoal.sistema.ordem_servico.service.ObjetoService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -18,9 +23,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ObjetoController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -60,6 +67,70 @@ class ObjetoControllerTest {
         String listaExperadaJSON = mapper.writeValueAsString(this.listaObjetos.stream().map(ObjetoDTO::new));
 
         assertThat(listaObjetosRetornoJSON).isEqualToIgnoringWhitespace(listaExperadaJSON);
+    }
+
+    @Test
+    void exibirAparelhos_deveriaRetornarExcecao_DBVazio() throws Exception {
+        when(serviceMock.exibirObjetos())
+                .thenThrow(new NenhumObjetoCadastradoException("Nenhum objeto encontrado no Banco de Dados!"));
+
+        mockMvc.perform(get(url))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.localizedMessage",
+                        is("Nenhum objeto encontrado no Banco de Dados!")));
+    }
+
+    @Test
+    void cadastrarObjeto_deveriaRetornarObjetoCadastrado() throws Exception {
+        Objeto objeto = this.listaObjetos.get(0);
+        ObjetoCadastradoForm formCadastro = converterObjetoEmFormCadastro(objeto);
+
+        when(serviceMock.cadastrarObjeto(any())).thenReturn(objeto);
+
+        mockMvc.perform(post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(formCadastro)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.marca", is(objeto.getMarca())))
+                .andExpect(jsonPath("$.modelo", is(objeto.getModelo())));
+    }
+
+    @Test
+    void cadastrarObjeto_deveriaRetornarException_ObjetoExistente() throws Exception{
+        Objeto objeto = this.listaObjetos.get(2);
+        ObjetoCadastradoForm formCadastro = converterObjetoEmFormCadastro(objeto);
+
+        when(serviceMock.cadastrarObjeto(any()))
+                .thenThrow(new ObjetoExistenteException("Objeto já existe no Banco de Dados."));
+
+        mockMvc.perform(post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(formCadastro)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.localizedMessage",
+                        is("Objeto já existe no Banco de Dados.")));
+    }
+
+    @Test
+    void cadastrarObjeto_deveriaRetornarException_ErroBanco() throws Exception{
+        Objeto objeto = this.listaObjetos.get(2);
+        ObjetoCadastradoForm formCadastro = converterObjetoEmFormCadastro(objeto);
+
+        when(serviceMock.cadastrarObjeto(any()))
+                .thenThrow(new IllegalArgumentException("Impossível salvar este usuário."));
+
+        mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(formCadastro)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.localizedMessage",
+                        is("Impossível salvar este usuário.")));
+    }
+
+    private ObjetoCadastradoForm converterObjetoEmFormCadastro(Objeto objeto) {
+        ObjetoCadastradoForm form =
+                new ObjetoCadastradoForm(objeto.getMarca(), objeto.getModelo());
+        return form;
     }
 
 }
