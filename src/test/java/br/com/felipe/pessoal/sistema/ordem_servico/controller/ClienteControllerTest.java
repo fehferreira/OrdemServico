a522380eade1062d9bfac22dededc66ee3d7c6a7
fehferreira <felipe.ferreira1003@gmail.com>
2021-06-30 12:16:49 -0300

    Finishing first test method

--- src/test/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/ClienteControllerTest.java
+++ src/test/java/br/com/felipe/pessoal/sistema/ordem_servico/controller/ClienteControllerTest.java
@@ -1,5 +1,6 @@
 package br.com.felipe.pessoal.sistema.ordem_servico.controller;
 
+import br.com.felipe.pessoal.sistema.ordem_servico.controller.dto.ClienteDto;
 import br.com.felipe.pessoal.sistema.ordem_servico.modelo.Cliente;
 import br.com.felipe.pessoal.sistema.ordem_servico.service.ClienteService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,9 +12,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -34,13 +37,19 @@ class ClienteControllerTest {
     private ClienteService serviceMock;
 
     @Test
-    void criaUmaRequisicaoParaListaDeClientes_retornaUmaListaDeClienteDTO() throws Exception{
+    void listarClientes_deveriaRetornarUmaListaClientesDto() throws Exception{
         List<Cliente> clientes = criarListaClientes();
 
         when(serviceMock.listarClientes(any())).thenReturn(clientes);
 
-        mockMvc.perform(get("/clientes"))
-                .andExpect(status().isOk());
+        MvcResult mvcResult = mockMvc.perform(get("/clientes"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String listaClientesJsonExperada = mapper.writeValueAsString(clientes.stream().map(ClienteDto::new));
+        String retornoRequisicaoJson = mvcResult.getResponse().getContentAsString();
+
+        assertThat(retornoRequisicaoJson).isEqualToIgnoringWhitespace(listaClientesJsonExperada);
     }
 
     private List<Cliente> criarListaClientes(){
